948aff35f1cfa87f68121147d6782bdfe6316465
RDArtemis <144180579+RDArtemis@users.noreply.github.com>
2025-03-18 21:31:40 -0700

    added sfx

--- Assets/_Scripts/Logic/Ball.cs
+++ Assets/_Scripts/Logic/Ball.cs
@@ -23,6 +23,14 @@ public class Ball : MonoBehaviour
             rb.angularVelocity = Vector3.zero;
             rb.AddForce(directionToFire * returnSpeed, ForceMode.Impulse);
         }
+        if (other.gameObject.CompareTag("Paddle") || other.gameObject.CompareTag("Environment"))
+        {
+            AudioManager.instance.playSound(AudioManager.instance.bounceClip);
+        }
+        else if (other.gameObject.CompareTag("Bricks"))
+        {
+            AudioManager.instance.playSound(AudioManager.instance.hitClip);
+        }
     }
 
     public void ResetBall()
