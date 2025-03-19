91c97f13f0d002431fd2da2252c47b40f4f0f443
RDArtemis <144180579+RDArtemis@users.noreply.github.com>
2025-03-18 21:24:19 -0700

    Added AudioManager and bgm

---
+++ Assets/_Scripts/AudioManager.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    [Header("Audio Sources")]
+
+    public AudioSource sfxSource;
+    public AudioSource ambientSource;
+
+
+    [Header("Audio Clips")]
+
+    public AudioClip bounceClip;
+    public AudioClip hitClip;
+    public AudioClip ambientClip;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+    void Start()
+    {
+        playAmbientSound();
+    }
+
+    public void playAmbientSound()
+    {
+
+
+        if (ambientClip != null && ambientSource != null)
+        {
+            ambientSource.clip = ambientClip;
+            ambientSource.loop = true;
+            ambientSource.Play();
+
+        }
+    }
+
+    public void playSound(AudioClip clip)
+
+    {
+        if (clip != null)
+
+        {
+            sfxSource.PlayOneShot(clip);
+        }
+    }
+
+
+
+
+
+
+}
+
+
